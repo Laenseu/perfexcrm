ecfb164558bc5184e87818c16eb631c70568d112
LAENSEU <62004618+Laenseu@users.noreply.github.com>
2023-11-06 09:03:46 +0800

    update

--- application/controllers/admin/Sidebar_controller.php
+++ application/controllers/admin/Sidebar_controller.php
@@ -21,8 +21,8 @@ class Sidebar_controller extends CI_Controller {
         $staff_id = $this->session->userdata('staff_user_id');
         $data['total_billable_hours'] = $this->sidebar_model->get_total_billable_hours($staff_id);
         $data['total_hours'] = $this->sidebar_model->get_total_hours($staff_id);
-        $data['total_Notbillable_hours'] = $this->sidebar_model ->get_total_Notbillable_hours($staff_id);
-        $data['sidebar_content'] = $this->load->view('admin/sidebar/sidebar_content', '', TRUE);
+        $data['total_Notbillable_hours'] = $this->sidebar_model->get_total_Notbillable_hours($staff_id);
+        $data['sidebar_contentZ'] = $this->load->view('admin/sidebar/sidebar_content', '', TRUE);
         $this->load->view('main_view', $data);
 
         
